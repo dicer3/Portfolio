aaf81305a7c730a4573cf7ea3e4317a9609a1bbb
pragungandotra@gmail.com <pragungandotra7@gmail.com>
2021-09-18 22:30:41 +0530

    last tuchups

--- src/components/data/projects_data.js
+++ src/components/data/projects_data.js
@@ -1,47 +1,54 @@
-import posts from '../../assets/projects_images/postSharer.PNG'
-import expensify from '../../assets/projects_images/expensifyApp.PNG'
-import devConnector from '../../assets/projects_images/devConnector.PNG'
-import jobs from '../../assets/projects_images/jobSchedulingSys.PNG'
-import intigue from '../../assets/projects_images/intigue.PNG'
+import posts from "../../assets/projects_images/postSharer.PNG";
+import expensify from "../../assets/projects_images/expensifyApp.PNG";
+import devConnector from "../../assets/projects_images/devConnector.PNG";
+import jobs from "../../assets/projects_images/jobSchedulingSys.PNG";
+import intigue from "../../assets/projects_images/intigue.PNG";
+import meetups from "../../assets/projects_images/travel meetups.PNG";
 
 const data_projects = [
-    {
-        name: 'Expensify-App',
-        image: expensify,
-        deployed_url: 'https://react2-expensify-app.herokuapp.com/',
-        github_url: 'https://github.com/dicer3/expensify-app',
-        category: ['react.js']
-    },
+  {
+    name: "Expensify-App",
+    image: expensify,
+    deployed_url: "https://react2-expensify-app.herokuapp.com/",
+    github_url: "https://github.com/dicer3/expensify-app",
+    category: ["react.js"],
+  },
 
-    {
-        name: 'Dev Connector',
-        image: devConnector,
-        deployed_url: 'https://frozen-temple-21009.herokuapp.com/',
-        github_url: 'https://github.com/dicer3/devConnector',
-        category: ['node.js', 'mongoDB', 'react.js']
-    },
+  {
+    name: "Dev Connector",
+    image: devConnector,
+    deployed_url: "https://frozen-temple-21009.herokuapp.com/",
+    github_url: "https://github.com/dicer3/devConnector",
+    category: ["node.js", "mongoDB", "react.js"],
+  },
 
-    {
-        name: 'Job Scheduling System',
-        image: jobs,
-        deployed_url: 'https://murmuring-river-18742.herokuapp.com/ ',
-        github_url: 'https://github.com/dicer3/Job-Scheduling-System ',
-        category: ['node', 'socket.io', 'react']
-    },
-
-    {
-        name: 'Post Sharer',
-        image: posts,
-        deployed_url: 'https://post-share-app.netlify.app/',
-        github_url: 'https://github.com/dicer3/MEAN_course',
-        category: ['django', 'react']
-    },
-    {
-        name: 'Work Sample(Design)',
-        image: intigue,
-        deployed_url: 'https://design-work-sample.netlify.app/',
-        category: ['html_css', 'vanilla']
-    }
-]
+  {
+    name: "Job Scheduling System",
+    image: jobs,
+    deployed_url: "https://murmuring-river-18742.herokuapp.com/ ",
+    github_url: "https://github.com/dicer3/Job-Scheduling-System ",
+    category: ["node", "socket.io", "react"],
+  },
+  {
+    name: "Post Sharer",
+    image: posts,
+    deployed_url: "https://post-share-app.netlify.app/",
+    github_url: "https://github.com/dicer3/MEAN_course",
+    category: ["django", "react"],
+  },
+  {
+    name: "Travel Meetups",
+    image: meetups,
+    deployed_url: "https://travel-meetups.vercel.app/",
+    github_url: "https://github.com/dicer3/Travel-Meetups",
+    category: ["django", "react"],
+  },
+  {
+    name: "Work Sample(Design)",
+    image: intigue,
+    deployed_url: "https://design-work-sample.netlify.app/",
+    category: ["html_css", "vanilla"],
+  },
+];
 
 export default data_projects;
--- src/components/data/resume_data.js
+++ src/components/data/resume_data.js
@@ -1,89 +1,66 @@
-import react from '../../assets/icons/react.svg'
-import typescript from '../../assets/icons/typescript.svg'
-import python from '../../assets/icons/python.svg'
-import node from '../../assets/icons/node.svg'
-import redux from '../../assets/icons/redux.svg'
-import angular from '../../assets/icons/angularjs.svg'
-import bootstrap from '../../assets/icons/bootstrap.svg'
-import scss from '../../assets/icons/scss.svg'
-import graphql from '../../assets/icons/graphql.svg'
-import jquery from '../../assets/icons/jquery.svg'
+import react from "../../assets/icons/react.svg";
+import typescript from "../../assets/icons/typescript.svg";
+import python from "../../assets/icons/python.svg";
+import node from "../../assets/icons/node.svg";
+import redux from "../../assets/icons/redux.svg";
+import angular from "../../assets/icons/angularjs.svg";
+import bootstrap from "../../assets/icons/bootstrap.svg";
+import scss from "../../assets/icons/scss.svg";
+import graphql from "../../assets/icons/graphql.svg";
+import nextJS from "../../assets/icons/next-logo.png";
+import jquery from "../../assets/icons/jquery.svg";
 
 const languages = [
-    {
-        icon: react,
-        name: 'React.js',
-        level: '90'
-    },
-    {
-        icon: redux,
-        name: 'Redux',
-        level: '85'
-    },
-    {
-        icon: node,
-        name: 'Node.js',
-        level: '80'
-    },
-    {
-        icon: typescript,
-        name: 'TypeScript',
-        level: '80'
-    },
-    {
-        icon: angular,
-        name: 'Angular',
-        level: '68'
-    },
-    {
-        icon: bootstrap,
-        name: 'Bootstrap',
-        level: '88'
-    },
-    {
-        icon: python,
-        name: 'Python/Flask',
-        level: '75'
-    },
-    {
-        icon: scss,
-        name: 'CSS/SCSS',
-        level: '85'
-    },
-    {
-        icon: jquery,
-        name: 'jQuery',
-        level: '85'
-    },
-    // {
-    //     icon: graphql,
-    //     name: 'graphql',
-    //     level: '85'
-    // }
-]
+  {
+    icon: react,
+    name: "React.js",
+    level: "90",
+  },
+  {
+    icon: redux,
+    name: "Redux",
+    level: "85",
+  },
+  {
+    icon: node,
+    name: "Node.js",
+    level: "80",
+  },
+  {
+    icon: typescript,
+    name: "TypeScript",
+    level: "80",
+  },
+  {
+    icon: angular,
+    name: "Angular",
+    level: "68",
+  },
+  {
+    icon: bootstrap,
+    name: "Bootstrap",
+    level: "88",
+  },
+  //   {
+  //     icon: python,
+  //     name: "Python/Flask",
+  //     level: "75",
+  //   },
+  {
+    icon: scss,
+    name: "CSS/SCSS",
+    level: "85",
+  },
+  {
+    icon: nextJS,
+    name: "Next.JS",
+    level: "70",
+  },
+  {
+    icon: graphql,
+    name: "GraphQL",
+    level: "65",
+  },
+];
 
-const tools = [
-    {
-        icon: react,
-        name: 'Figma',
-        level: '85'
-    },
-    {
-        icon: react,
-        name: 'Photoshop',
-        level: '45'
-    },
-    {
-        icon: react,
-        name: 'Illustrator',
-        level: '60'
-    },
-    {
-        icon: react,
-        name: 'Framer',
-        level: '45'
-    },
-
-]
-
-export { tools, languages }
+export { languages };
